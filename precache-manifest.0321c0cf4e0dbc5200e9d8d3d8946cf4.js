736a6ea0ba1d87362f86e0e0019ae11838635a0b
Anderson Luan <anderson@Andersons-MacBook-Pro.local>
2019-10-30 10:31:23 -0400

    Updates

---
+++ precache-manifest.0321c0cf4e0dbc5200e9d8d3d8946cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3048db4b82539007de00437277d36b",
+    "url": "/Anderson/index.html"
+  },
+  {
+    "revision": "473f0c0268ea1f4faee1",
+    "url": "/Anderson/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3db6c6e2b9e1d4597b89",
+    "url": "/Anderson/static/js/2.946ea795.chunk.js"
+  },
+  {
+    "revision": "473f0c0268ea1f4faee1",
+    "url": "/Anderson/static/js/main.c754e25c.chunk.js"
+  },
+  {
+    "revision": "b6da496156a7f7c2d1d3",
+    "url": "/Anderson/static/js/runtime-main.fc0e8a82.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Anderson/static/media/logo.25bf045c.svg"
+  }
+]);
